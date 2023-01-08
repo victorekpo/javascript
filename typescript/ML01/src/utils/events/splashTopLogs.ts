d18b18e937cbd9962e78397e3060633d59799222
Vic <victor.ekpo@gmail.com>
2023-01-08 17:34:45 -0600

    util.promisify

--- typescript/ML01/src/utils/events/splashTopLogs.ts
+++ typescript/ML01/src/utils/events/splashTopLogs.ts
@@ -2,15 +2,15 @@ import { runCommand } from "../exec/runCommand";
 
 export const getSplashTopLogs = async () => {
     const fullResult: any = await runCommand('PowerShell.exe "Get-WinEvent -MaxEvents 1 -FilterHashtable @{Logname=\'Splashtop-Splashtop Business app-Remote Computer Info/Operational\'} | ConvertTo-Json"', true);
+    console.log(fullResult, "RESULT")
 
     const result = {
-        time: fullResult.value['TimeCreated'],
-        machineName: fullResult.value['MachineName'],
-        message: fullResult.value['Message'],
-        properties: fullResult.value['Properties'],
-        processID: fullResult.value['ProcessId'],
-        userID: fullResult.value['UserId'],
-        level: fullResult.value['Level']
+        time: fullResult['TimeCreated'],
+        machineName: fullResult['MachineName'],
+        message: fullResult['Message'],
+        properties: fullResult['Properties'],
+        processID: fullResult['ProcessId'],
+        level: fullResult['Level']
     }
     console.log(result);
     return result;
--- typescript/ML01/src/utils/exec/runCommand.ts
+++ typescript/ML01/src/utils/exec/runCommand.ts
@@ -1,11 +1,37 @@
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
-
-export const runCommand = async (command: string, json: boolean = false) => {
-    const { stdout, stderr, error } = await exec(command);
-    return {
-        value: json ? JSON.parse(stdout) : stdout,
-        stderr,
-        error
-    }
+export const runCommand = async (command: string, json: boolean = false): Promise<any> => {
+    return new Promise((resolve, reject) => {
+        const exec = require("child_process").exec;
+        exec(
+            command,
+            (
+                error: Error,
+                stdout: string | Buffer,
+                stderr: string | Buffer
+            ) => {
+                if (error) {
+                    reject(error);
+                    return;
+                }
+                if (stderr) {
+                    reject(stderr);
+                    return;
+                } else {
+                    resolve(json ? JSON.parse(String(stdout)) : stdout);
+                }
+            }
+        );
+    });
 };
+
+
+// ALTERNTATIVE WITH UTIL.PROMISIFY
+// export const runCommand = async (command: string, json: boolean = false) => {
+//     const util = require('util');
+//     const exec = util.promisify(require('child_process').exec);
+//     const { stdout, stderr, error } = await exec(command);
+//     return {
+//         value: json ? JSON.parse(stdout) : stdout,
+//         stderr,
+//         error
+//     }
+// };
