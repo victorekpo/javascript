c6af6339d5290fe4429ad1d45c526adf69ecd1f8
Vic <victor.ekpo@gmail.com>
2023-01-12 05:26:30 -0600

    finally got it to recognize which file got updated in quickRead!

--- typescript/ML01/src/classes/bots.ts
+++ typescript/ML01/src/classes/bots.ts
@@ -167,19 +167,27 @@ export class Robot implements Machine {
                 this.brain[type][key] = [];
             const itemExists = this.brain[type]?.[key]?.find(({value: val}: any) => ([val].flat().join('~') == [formattedValue].flat().join('~')
                     || val === formattedValue || val === value))
-                || formattedValue.split('\r\n').every((line:any) => this.brain[type][key].includes({value:line})
+                || formattedValue.split('\r\n').filter((x: any) => !x.includes('@file')).every((line:any) => {
+                    line = line.replace(' @end:','')
+                    console.log('LINE', line,this.brain[type][key].includes({value:line}))
+                    return this.brain[type][key].includes({value:line})
                     || this.brain[type][key].reduce((exists: any,check: any) => {
                         if(check['value'].split('\r\n').find((i: any) => i === line))
                             exists = true;
                         return !!exists
                     },false)
-                )
+                    || this.brain[type][key].reduce((exists: any,check: any) => {
+                        if(check['value'].split(/(\@file:.*\r\n|@file:.*\....|@end:)/).filter((x: any, i: number, arr: any)=> x !== '' && x !== '\r\n' && (!arr[i]?.includes('@file') || arr[i+1])).find((i: any) => i.includes(line)))
+                            exists = true;
+                        return !!exists
+                    },false)
+                })
             console.log('exists?',!!itemExists);
             if(!itemExists && formattedValue.length === 1 && this.brain[type][key]?.length < 1)
                 this.brain[type][key] = [{time: new Date(), value}];
             if(!itemExists && (formattedValue.length > 1 || (typeof(tryToParse(this.brain[type][key]?.[value])) === 'object'))) {
                 const toAdd = [formattedValue].flat().join('~').split(/\r\n|~/)
-                    .filter(x => { return x.includes('@@') ? true : !this.brain[type][key]
+                    .filter(x => { return x.includes('@file:') ? true : !this.brain[type][key]
                         .find(({value}: any) => {
                            // console.log("COMPARISON",value,"COMPARISON 2", x);
                            //  console.log("term found",value.split('\r\n')
@@ -187,7 +195,7 @@ export class Robot implements Machine {
                             return x === value || (value.split('\r\n')
                                 .find((y: any)=> y===x))
                         })
-                    }).join('\r\n') || null
+                    }).join('\r\n').split(/(\@file:.*\r\n|@file:.*\....|@end:)/).filter((x,i,arr)=> x !== '' && x !== '\r\n' && (!arr[i]?.includes('@file') || arr[i+1])).join('\r\n') || null
                 console.log('value TO ADD',toAdd)
                 if(toAdd) {
                     this.brain[type][key] = Array.from(new Set([(this.brain[type][key].length > 0 ? this.brain[type][key] : []),{time: new Date(), value: toAdd}].flat()))
