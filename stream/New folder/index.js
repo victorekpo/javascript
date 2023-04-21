0fe5e6b55de249cc1c914145ee417cfb8021dbc8
Vic <victor.ekpo@gmail.com>
2023-04-21 06:28:51 -0500

    working example of jsonstreamer

--- stream/New folder/index.js
+++ stream/New folder/index.js
@@ -1,13 +1,24 @@
 const request = require('request')
-    , JSONStream = require('JSONStream')
-    , es = require('event-stream')
+const JSONStream = require('JSONStream')
+const es = require('event-stream')
 
-const parser = JSONStream.parse('id') //emit parts that match this path (any element of the rows array)
-    , req = request({url: 'https://raw.githubusercontent.com/json-iterator/test-data/master/large-file.json'})
-    , logger = es.mapSync(function (data) {  //create a stream that logs to stderr,
+// console.time('json-streamer')
+
+const parser = JSONStream.parse('..id') //emit parts that match this path (any element of the rows array)
+    // . - matches current path
+    // rows - matches { rows: .. }
+    // rows.docs - matches { rows: { docs: .. } }
+    // ..rows - matches [{rows: ...}]
+    // ..rows.docs - matches [ { rows: { docs: ... } }]
+
+const req = request({url: 'https://raw.githubusercontent.com/json-iterator/test-data/master/large-file.json'})
+
+const logger = es.mapSync(function (data) {  //create a stream that logs to stderr,
     console.log(data)
     return data
 })
 
 req.pipe(parser)
 parser.pipe(logger)
+
+// console.timeEnd('json-streamer')
