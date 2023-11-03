a7141bf2ca2d4165fa14b193ea2629effd32e432
Vic <victor.ekpo@gmail.com>
2023-11-03 04:36:25 -0500

    chore: update

--- socket-io/demos/chat/public/main.js
+++ socket-io/demos/chat/public/main.js
@@ -2,9 +2,18 @@ $(function() {
   const FADE_TIME = 150; // ms
   const TYPING_TIMER_LENGTH = 400; // ms
   const COLORS = [
-    '#e21400', '#91580f', '#f8a700', '#f78b00',
-    '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
-    '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
+    '#e21400',
+    '#91580f',
+    '#f8a700',
+    '#f78b00',
+    '#58dc00',
+    '#287b00',
+    '#a8f07a',
+    '#4ae8c4',
+    '#3b88eb',
+    '#3824aa',
+    '#a700ff',
+    '#d300e7'
   ];
 
   // Initialize variables
